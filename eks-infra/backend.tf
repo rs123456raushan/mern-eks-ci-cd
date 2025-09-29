35524d574e98cf28cad788f8470fb39c9d2bb246
rs123456raushan <rs123456raushan@gmail.com>
2025-09-29 22:15:31 +0530

    updated bucket

--- eks-infra/backend.tf
+++ eks-infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-tf-state-backend-s3"
+    bucket = "terraform-tf-state-bucket-s3"
     key    = "eks/terraform.tfstate"
     region = "eu-north-1"
   }
--- jenkins-infra/backend.tf
+++ jenkins-infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-tf-state-backend-s3"
+    bucket = "terraform-tf-state-bucket-s3"
     key    = "jenkins/terraform.tfstate"
     region = "eu-north-1"
   }
